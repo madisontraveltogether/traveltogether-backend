0cd8b0f9957d363b0d61e1ac4b9075abe23f6e97
madisontraveltogether <madison@gettraveltogether.com>
2024-11-17 20:33:22 +0000

    userController added to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const userController = require('../controllers/userController');
 
 const router = express.Router();
 
