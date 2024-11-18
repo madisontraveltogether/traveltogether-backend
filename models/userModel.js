77bd6fc527554e91d16308899109ac3c84cd5d0e
madisontraveltogether <madison@gettraveltogether.com>
2024-11-18 17:25:48 +0000

    deleting something from the user schema

--- models/userModel.js
+++ models/userModel.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, match: /.+\@.+\..+/ }, // Validate email format
-  password: { type: String, required: true }, // Store hashed password
+  password: { type: String, required: true }, 
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
   refreshToken: { type: String },
   joinedTrips: [{ type: Schema.Types.ObjectId, ref: 'Trip' }],
@@ -20,23 +20,6 @@ const UserSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
   deletedAt: { type: Date }, // For soft deletion
 });
-
-// Pre-save hook to hash passwords
-UserSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Method to compare passwords
-UserSchema.methods.comparePassword = async function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-};
 
 module.exports = mongoose.model('User', UserSchema);
 
