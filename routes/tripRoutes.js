f9277afb607d8bc82a746fe40b9778e1a5e001c0
Madison <mhmadisonhenry@gmail.com>
2024-11-25 20:58:15 -0500

    last for the night

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -18,7 +18,7 @@ const userController = require('../controllers/userController');
 const router = express.Router();
 const { validateTask } = require('../middlewares/taskValidation');
 const notificationController = require('../controllers/notificationController');
-console.log(notificationController);
+
 /**
  * Trip Routes
  */
